8cb8114915bd0f7f990665201786bc1e6eb2f38b
kriti.kumari <kriti.cs10@gmail.com>
2022-12-31 15:21:25 +0100

    Rabbitmq host updated

--- simulator/settings.py
+++ simulator/settings.py
@@ -9,7 +9,7 @@ POWER_METER_QUEUE = os.environ.get('POWER_METER_QUEUE', 'power_meter_reading')
 SIMULATOR_MIN_WEIGHT = float(os.environ.get('SIMULATOR_MIN_WEIGHT', '0.2'))
 SIMULATOR_MAX_WEIGHT = float(os.environ.get('SIMULATOR_MAX_WEIGHT', '0.5'))
 
-RABBIT_MQ_HOST = os.environ.get('RABBIT_MQ_HOST', 'amqp://guest:guest@localhost:5672')
+RABBIT_MQ_HOST = os.environ.get('RABBIT_MQ_HOST', 'amqp://guest:guest@rabbitmq:5672')
 
 LOG_DIR_PATH = pathlib.Path(__file__).parent.parent.joinpath('logs')  # if you change this, also change in docker-compose
 print(LOG_DIR_PATH)
